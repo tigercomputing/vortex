d77479835e6f8a3f48c4c7ca4d63d32681651952
Chris Boot <bootc@bootc.net>
2016-03-24 19:08:44 +0000

    vortex.bootstrap: add more documentation

--- src/vortex/bootstrap.py
+++ src/vortex/bootstrap.py
@@ -15,6 +15,44 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Vortex 1st stage bootstrap functionality.
+
+This file is a stand-alone executable Python script that is designed to be
+called by the "Stage 0" bootstrap (e.g. Cloud-Init). Its tasks are to:
+
+* Locate and parse the Vortex configuration file.
+* Download the rest of Vortex from a distribution source.
+* Validate the downloaded file's cryptographic signature (TODO).
+* Load the downloaded Vortex package and run the next stage of the bootstrap
+  process.
+
+All of the above must be done using only the Python standard library, and must
+be fully compatible with Python 2.6 or higher, and Python 3.3 or higher.
+
+The following configuration options are *required*:
+
+``[bootstrap].source``
+   The source URI to download Vortex from. This can be any URI supported by
+   Python's :mod:`urllib` (or :mod:`urllib.request` in Python 3+). The URI must
+   point at a Python Egg file containing the rest of the Vortex code.
+
+The following configuration options are *optional*:
+
+``[bootstrap].entry`` = ``vortex:stage2``
+   The entry point function to call after having downloaded the Egg file. By
+   default this points at :func:`vortex.stage2`, which is used to continue the
+   bootstrap process.
+
+Some of the code in this file is made up of simpler / stripped
+re-implementations of code found elsewhere in Vortex, or even from parts of
+:mod:`six`.
+
+.. note:: A bug in Sphinx (1641_) prevents some of the members in this module
+          from having their documentation generated.
+
+.. _1641: https://github.com/sphinx-doc/sphinx/issues/1641
+"""
 
 from __future__ import absolute_import, print_function, unicode_literals
 
@@ -53,12 +91,16 @@ except ImportError:
         __import__(name)
         return sys.modules[name]
 
-# Configuration file path
+#: Path to Vortex configuration file. Can be overridden using the
+#: ``VORTEX_INI`` environment variable.
 VORTEX_INI = '/etc/vortex.ini'
 VORTEX_INI = os.environ.get('VORTEX_INI', VORTEX_INI)
 
-# Some globals we'll refer to in a few places
+#: Parsed configuration. See :func:`read_config`.
 config = SafeConfigParser()
+
+#: Path to a temporary directory. Automatically deleted at exit (see
+#: :func:`cleanup_tmpdir`).
 tmpdir = tempfile.mkdtemp(prefix='vortex-')
 
 # Configuration defaults
@@ -70,13 +112,15 @@ config.set('bootstrap', 'entry', 'vortex:stage2')
 def cleanup_tmpdir():
     """
     Delete our temporary directory at exit.
+
+    Called when the Python interpreter exits using :func:`atexit.register`.
     """
     shutil.rmtree(tmpdir)
 
 
 def die(message, exit=1):
     """
-    Simple wrapper to print to stderr and exit with a failure return.
+    Simple wrapper to print to ``stderr`` and exit with a failure return.
     """
     print(message, file=sys.stderr)
     sys.exit(exit)
@@ -84,7 +128,7 @@ def die(message, exit=1):
 
 def read_config():
     """
-    Read the vortex.ini configuration file and perform sanity checks.
+    Read the ``vortex.ini`` configuration file and perform sanity checks.
     """
     read = config.read(VORTEX_INI)
     if not read:
@@ -97,9 +141,12 @@ def read_config():
 
 def fetch_vortex():
     """
-    Download the Vortex egg file.
+    Download the Vortex Egg file.
 
-    .. todo:: Validate signature somehow.
+    Returns the full path to the downloaded file, which will be within the
+    :data:`tmpdir` directory.
+
+    .. todo:: Validate cryptographic signature.
     """
     url = config.get('bootstrap', 'source')
     o = urlparse(url)
@@ -144,6 +191,9 @@ def execute_vortex(filepath):
 
 
 def main():
+    """
+    Main entry point.
+    """
     read_config()
     filepath = fetch_vortex()
     execute_vortex(filepath)
