0c449d229aa675788eebefe44c283466af8cb883
Chris Boot <crb@tiger-computing.co.uk>
2016-04-01 10:50:56 +0100

    vortex.utils: new module containing helpful utilities
    Currently contains a copy of the `cached_property` decorator class from
    Django.

---
+++ src/vortex/utils.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015  Tiger Computing Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+# class cached_property is
+# Copyright (c) Django Software Foundation and individual contributors.
+# All rights reserved.
+# License: BSD
+# Source: django.utils.functional.cached_property
+class cached_property(object):
+    """
+    Decorator that converts a method with a single self argument into a
+    property cached on the instance.
+
+    Optional ``name`` argument allows you to make cached properties of other
+    methods. (e.g.  url = cached_property(get_absolute_url, name='url') )
+    """
+    # This entire class was obtained from Django (django/util/functional.py),
+    # which is licensed under a 3-clause BSD license.
+    # Copyright (c) Django Software Foundation and individual contributors.
+    def __init__(self, func, name=None):
+        self.func = func
+        self.__doc__ = getattr(func, '__doc__')
+        self.name = name or func.__name__
+
+    def __get__(self, instance, type=None):
+        if instance is None:
+            return self
+        res = instance.__dict__[self.name] = self.func(instance)
+        return res
