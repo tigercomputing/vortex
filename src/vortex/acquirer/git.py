a06453163b995a088532a2d0ef902bf4157b3bcc
Chris Boot <bootc@bootc.net>
2016-04-08 12:18:20 +0100

    vortex.acquirer.git: implement Git acquirer
    Vortex can now successfully clone a Git repo and obtain the requested
    revision from it.

--- src/vortex/acquirer/git.py
+++ src/vortex/acquirer/git.py
@@ -17,10 +17,14 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
+import contextlib
 import logging
+import os
+import os.path
 
-from vortex.acquirer import Acquirer
+from vortex.acquirer import Acquirer, AcquisitionError
 from vortex.config import cfg
+from vortex.environment import install_package, runcmd
 
 
 logger = logging.getLogger(__name__)
@@ -47,6 +51,30 @@ class GitAcquirer(Acquirer):
            understood by ``git checkout``. Note that in many cases, the
            resulting payload may end up on a "detached HEAD".
     """
+    #: Path to the git binary
+    GIT = '/usr/bin/git'
+
+    #: Package to install. Passed to
+    #: :func:`vortex.environment.install_package` if Git needs installing.
+    GIT_PKG = 'git'
+
+    def __init__(self, section):
+        super(GitAcquirer, self).__init__(section)
+        self.__check_installed()
+
+    def __check_installed(self):
+        # If the executable exists, that's plenty good enough.
+        if os.path.isfile(self.GIT):
+            return
+
+        install_package(self.GIT_PKG)
+
+        if os.path.isfile(self.GIT):
+            return
+
+        raise AcquisitionError(
+            "Cannot find git binary after installing the git package")
+
     def configure(self):
         """
         Configure this acquirer based on settings from the vortex
@@ -64,6 +92,23 @@ class GitAcquirer(Acquirer):
         # Validate the configuration and absorb the values into this object
         cfg.absorb(self, self.section, required, defaults)
 
+    @contextlib.contextmanager
+    def __git_helper(self, cwd):
+        # We're going to run Git several times, so let's hide the complexity in
+        # this context manager. It just returns a function that can mangle the
+        # arguments and call runcmd() for us, then check that everything went
+        # OK.
+        def git_wrapper(*args):
+            command = [self.GIT] + list(args)
+            (ret, out) = runcmd(command, cwd=cwd)
+            if ret == 0:
+                return
+
+            raise AcquisitionError("Failed to acquire Git repo {repo}".format(
+                repo=self.repository), out)
+
+        yield git_wrapper
+
     def acquire_into(self, directory):
         """
         Perform the resource acquisition into the given directory.
@@ -72,3 +117,12 @@ class GitAcquirer(Acquirer):
         """
         logger.info("Acquiring Git repo {repo} rev {rev} into {dir}".format(
             dir=directory, repo=self.repository, rev=self.revision))
+
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+        with self.__git_helper(directory) as git:
+            git('init')
+            git('remote', 'add', 'origin', self.repository)
+            git('fetch', 'origin', self.revision)
+            git('checkout', 'FETCH_HEAD')
